629b7a267b8b1840fb2e6c3054cc4f0504672d4f
knjmoo <57110372+knjmoo@users.noreply.github.com>
2019-12-19 15:27:49 -0600

    models

---
+++ kbd/models.py
@@ -0,0 +1,78 @@
+#from kbd import db
+from datetime import datetime
+
+
+class CashierEfficiency(db.Model):
+    __tablename__='ce'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    tm_name=db.Column(db.String(50))
+    tm_sales=db.Column(db.Float)
+    tm_minutes=db.Column(db.Integer)
+
+    def __init__(self,title,text,related_deck,related_box,related_card):
+        self.title=title
+        self.text=text
+        self.related_deck=related_deck
+        self.related_box=related_box
+        self.related_card=related_card
+
+class GameFilm(db.Model):
+    __tablename__='gamefilm'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    tm_name=db.Column(db.String(50))
+    score=db.Column(db.Integer)
+
+class ToGoLabel(db.Model):
+    __tablename__='tgl'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    time_measure=db.Column(db.DateTime(40))
+    tm_name=db.Column(db.String(50))
+    score=db.Column(db.String(25))
+
+class GMTimes(db.Model):
+    __tablename__='gmtimes'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    time_measure=db.Column(db.DateTime(40))
+    order_type=db.Column(db.String(20))
+    score=db.Column(db.Integer)
+    paid=db.Column(db.Boolean)
+
+class LineTimes(db.Model):
+    __tablename__='linetimes'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    am_taken=db.Column(db.Integer)
+    am_out=db.Column(db.Integer)
+    am_over_11=db.Column(db.Integer)
+    pm_taken=db.Column(db.Integer)
+    pm_out=db.Column(db.Integer)
+    pm_over_11=db.Column(db.Integer)
+
+class TacoTimes(db.Model):
+    __tablename__='tacotimes'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    time_measure=db.Column(db.DateTime(40))
+    score=db.Column(db.DateTime(40))
