fc8d66cca39e42436201b18c4b860f3885e5cee8
knjmoo <57110372+knjmoo@users.noreply.github.com>
2019-12-19 15:40:05 -0600

    models

--- kbd/models.py
+++ kbd/models.py
@@ -81,12 +81,41 @@ class BagTimes(db.Model):
     __tablename__='bagtimes'
     id=db.Column(db.Integer,primary_key=True)
     week_ending=db.Column(db.DateTime(40))
-    fiscal_week=db.Column(db.Integer)
+    month_ending=db.Column(db.DateTime(40))
+    fiscal_month=db.Column(db.String(20))
     location=db.Column(db.String(20))
+    fiscal_week=db.Column(db.Integer)
     score=db.Column(db.DateTime(40))
+    score_met=db.Column(db.Float)
 
 class FoodVariance(db.Model):
     __tablename__='foodvariance'
+    id=db.Column(db.Integer,primary_key=True)
+    month_ending=db.Column(db.DateTime(40))
+    fiscal_month=db.Column(db.String(20))
+    location=db.Column(db.String(20))
+    score=db.Column(db.Float)
+
+class Inspections(db.Model):
+    __tablename__='inspections'
+    id=db.Column(db.Integer,primary_key=True)
+    week_ending=db.Column(db.DateTime(40))
+    fiscal_week=db.Column(db.Integer)
+    location=db.Column(db.String(20))
+    date_measured=db.Column(db.DateTime(40))
+    time_measure=db.Column(db.DateTime(40))
+    score=db.Column(db.Float(40))
+
+class StoreAssessments(db.Model):
+    __tablename__='storeassessments'
+    id=db.Column(db.Integer,primary_key=True)
+    month_ending=db.Column(db.DateTime(40))
+    fiscal_month=db.Column(db.String(20))
+    location=db.Column(db.String(20))
+    score=db.Column(db.Float)
+
+class OrderAccuracy(db.Model):
+    __tablename__='orderaccuracy'
     id=db.Column(db.Integer,primary_key=True)
     month_ending=db.Column(db.DateTime(40))
     fiscal_month=db.Column(db.String(20))
