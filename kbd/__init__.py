4fe1c99123088235ff4772d054e1e89a48215015
knjmoo <57110372+knjmoo@users.noreply.github.com>
2019-12-19 11:48:32 -0600

    core blueprint

--- kbd/__init__.py
+++ kbd/__init__.py
@@ -2,3 +2,9 @@ import os
 from flask import Flask
 
 app=Flask(__name__)
+
+
+
+from kbd.core.views import core
+
+app.register_blueprint(core)
--- kbd/core/views.py
+++ kbd/core/views.py
@@ -1,1 +1,8 @@
 from kbd import app
+from flask import render_template,request,Blueprint,redirect,url_for
+
+core = Blueprint('core',__name__)
+
+@core.route('/')
+def index():
+    return render_template('index.html')
