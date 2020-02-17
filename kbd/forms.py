eb49ae2e7b751f9aab349c81a785527f73acfdb4
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-02-17 15:58:02 -0600

    forms

---
+++ kbd/forms.py
@@ -0,0 +1,7 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+from wtforms import ValidationError
+
+class SalesForm(FlaskForm):
+    submit=SubmitField('Add')
