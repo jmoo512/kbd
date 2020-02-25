35bd42b123bd26035dfc1b456259d64d92e03136
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-02-25 15:34:48 -0600

    update

--- kbd/static/charts.js
+++ kbd/static/charts.js
@@ -1,48 +1,19 @@
-window.addEventListener('load',setup);
+const ctx = document.getElementById('myChart').getContext('2d');
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
-async function setup(){
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const totalSales = await getData();
-  const myChart = new Chart(ctx, {
-    type: 'bar',
+    // The data for our dataset
     data: {
-      labels: totalSales.store,
-      datasets: [
-        {
-          label: 'Store Sales',
-          data: totalSales.sales,
-          fill: false,
-          borderColor: 'rgba(255,99,132,1)',
-          backgroundColor: 'rgba(255,99,132,0.5)',
-          borderWidth: 1
-        }
-      ]
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [{
+            label: 'My First dataset',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [0, 10, 5, 2, 20, 30, 45]
+        }]
     },
-    options: {
-      scales: {
-            yAxes: [{
-                ticks: {
-                    callback: function(value, index, values) {
-                        return '$' + value;
-                    }
-                }
-            }]
-        }
-    }
-  });
-}
 
-async function getData() {
-  const response = await fetch('static/sales.csv');
-  const data = await response.text();
-  const store = [];
-  const sales = [];
-  const rows = data.split('\n').slice(1,-1);
-  rows.forEach(row=> {
-    const cols = row.split(',');
-    console.log(cols);
-    store.push(cols[0]);
-    sales.push(parseFloat(cols[1]));
-  });
-  return {store, sales};
-}
+    // Configuration options go here
+    options: {}
+});
