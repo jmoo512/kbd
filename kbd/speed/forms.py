ead2a7d09c564484e2daf51e8fc33ea6b142e778
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-03-09 09:57:13 -0500

    field

--- kbd/speed/forms.py
+++ kbd/speed/forms.py
@@ -6,7 +6,7 @@ from wtforms.fields.html5 import DateField
 
 class CEForm(FlaskForm):
     week_ending=DateField('Week Ending', format='%Y-%m-%d')
-    fiscal_week=IntegerField('Fiscal Week')
+    fiscal_week=SelectField('Fiscal Week', choices=[('1', '1'), ('2','2'), ('3','3'), ('4','4'), ('5','5')])
     location=SelectField('Location', choices=[('North','North'),('South','South'),('Round Rock','Round Rock'),('620','620'),('Lamar','Lamar'),('MF1','MF1'),('MF2','MF2'),('MF3','MF3'),('MF4','MF4'),('MFT2','MF Trailer')])
     date_measured=DateField('Date Measured', format='%Y-%m-%d')
     tm_name=StringField('TM Name')
