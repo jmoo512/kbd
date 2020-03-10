6604b926d6c721d8916a31d69f234915b2539fd6
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-03-10 11:54:21 -0500

    db model

--- kbd/speed/models.py
+++ kbd/speed/models.py
@@ -6,6 +6,8 @@ class CashierEfficiency(db.Model):
     id=db.Column(db.Integer,primary_key=True)
     week_ending=db.Column(db.Date)
     fiscal_week=db.Column(db.Integer)
+    fiscal_month=db.Column(db.String(20))
+    fiscal_year=db.Column(db.Integer)
     location=db.Column(db.String(20))
     date_measured=db.Column(db.Date)
     tm_name=db.Column(db.String(50))
@@ -16,6 +18,8 @@ class CashierEfficiency(db.Model):
     def __init__(self, week_ending, fiscal_week, location, date_measured, tm_name, tm_sales, tm_minutes, tm_efficiency):
         self.week_ending=week_ending
         self.fiscal_week=fiscal_week
+        self.fiscal_month=fiscal_month
+        self.fiscal_year=fiscal_year
         self.location=location
         self.date_measured=date_measured
         self.tm_name=tm_name
