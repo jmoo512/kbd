d25077349047178d49e0334329e42556955df368
jmoo <pauperbattlebox@gmail.com>
2020-03-12 16:19:05 -0500

    ce models

--- kbd/speed/views.py
+++ kbd/speed/views.py
@@ -45,10 +45,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_two.data,
+                tm_sales=form.tm_sales_two.data,
+                tm_minutes=form.tm_minutes_two.data,
+                tm_efficiency=form.tm_efficiency_two.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
@@ -62,10 +62,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_three.data,
+                tm_sales=form.tm_sales_three.data,
+                tm_minutes=form.tm_minutes_three.data,
+                tm_efficiency=form.tm_efficiency_three.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
@@ -79,10 +79,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_four.data,
+                tm_sales=form.tm_sales_four.data,
+                tm_minutes=form.tm_minutes_four.data,
+                tm_efficiency=form.tm_efficiency_four.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
@@ -96,10 +96,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_five.data,
+                tm_sales=form.tm_sales_five.data,
+                tm_minutes=form.tm_minutes_five.data,
+                tm_efficiency=form.tm_efficiency_five.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
@@ -113,10 +113,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_six.data,
+                tm_sales=form.tm_sales_six.data,
+                tm_minutes=form.tm_minutes_six.data,
+                tm_efficiency=form.tm_efficiency_six.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
@@ -130,10 +130,10 @@ def add():
                 fiscal_year=form.fiscal_year.data,
                 location=form.location.data,
                 date_measured=form.date_measured.data,
-                tm_name=form.tm_name_one.data,
-                tm_sales=form.tm_sales_one.data,
-                tm_minutes=form.tm_minutes_one.data,
-                tm_efficiency=form.tm_efficiency_one.data,
+                tm_name=form.tm_name_seven.data,
+                tm_sales=form.tm_sales_seven.data,
+                tm_minutes=form.tm_minutes_seven.data,
+                tm_efficiency=form.tm_efficiency_seven.data,
                 mod_one=form.mod_one.data,
                 mod_two=form.mod_two.data
             )
