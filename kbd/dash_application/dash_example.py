ac051406f7cdc872aa89950ee2850c897e926bd2
jmoo <joshmoore@wesellmeat.com>
2020-03-24 11:40:10 -0500

    remove df_sales

--- kbd/dash_application/dash_example.py
+++ kbd/dash_application/dash_example.py
@@ -94,7 +94,7 @@ def init_callbacks(dash_app):
         Output(component_id='sales', component_property='figure'),
         [Input(component_id='store', component_property='value')])
     def update_sales(update_sales_chart):
-        df=df_sales[(df_sales['location']==update_sales_chart)]
+        df=df[(df['location']==update_sales_chart)]
         df['week_ending']=df['week_ending'].astype('datetime64[ns]')
         df.sort_values(by='week_ending', inplace=True)
         df.reset_index(inplace=True)
