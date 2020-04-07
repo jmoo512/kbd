56ebb88f02be4ea7cea1ff9fca0d3e6aec6af645
jmoo <joshmoore@wesellmeat.com>
2020-04-07 16:34:55 -0500

    chartjs

--- kbd/static/chart.js
+++ kbd/static/chart.js
@@ -1,30 +1,32 @@
 let salesAPI="http://127.0.0.1:5000/sales2018"
 
-getData()
 
 async function getData() {
   const response = await fetch(salesAPI);
   const data = await response.json();
-  console.log(data)
 
-  const y2018 = []
+  const tmpArray = []
 
   data.forEach( obj => {
     if(obj.location === '183') {
-      y2018.push(obj)
+      tmpArray.push(obj)
     }
   })
-  console.log(y2018)
-  return y2018
+  console.log(tmpArray)
+  return tmpArray
+
 }
 
 
+let y2018
+getData().then( result => y2018 = result ).catch( e => console.error(e))
+console.log(y2018)
 
 const chart = c3.generate({
     bindto: '#chart',
     data: {
       columns: [
-        ['2018', y2018],
+        ['2018', 50, 20, 10, 40, 15, 2],
         ['data2', 50, 20, 10, 40, 15, 25]
       ]
     }
