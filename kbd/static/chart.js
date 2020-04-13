8bb4acba338be47ce40fd27be74c4ed74565e5a8
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-04-13 15:58:56 -0500

    dynamic api fetch

--- kbd/static/chart.js
+++ kbd/static/chart.js
@@ -1,36 +1,36 @@
-let salesAPI="http://127.0.0.1:5000/sales2018/" + "183"
-
-async function getData() {
-  const response = await fetch(salesAPI);
-  const data = await response.json();
-
-  const tmpArray = [];
-
-  data.forEach( obj => {
-    tmpArray.push(obj.sales);
-  });
-  console.log(tmpArray);
-  return tmpArray;
-
-}
-
-let y2018;
-
-getData().then( tmpArray => y2018 = tmpArray ).catch( e => console.error(e));
-console.log(y2018);
-
-const chart = c3.generate({
-  bindto: '#chart',
-  data: {
-    columns: [
-      ['2018', 2,3,4,5,6]
-    ]
-  }
-});
-
 document.getElementById("store-select").addEventListener("change",selectStore);
+
 function selectStore() {
   let store = document.getElementById("store-select").value;
   document.getElementById("chosen-store").innerHTML = store + " selected";
-  return store
+
+  let salesAPI="http://127.0.0.1:5000/sales2018/" + store
+
+  async function getData() {
+    const response = await fetch(salesAPI);
+    const data = await response.json();
+
+    const tmpArray = [];
+
+    data.forEach( obj => {
+      tmpArray.push(obj.sales);
+    });
+    console.log(tmpArray);
+    return tmpArray;
+
+  }
+
+  let y2018;
+
+  getData().then( tmpArray => y2018 = tmpArray ).catch( e => console.error(e));
+  console.log(y2018);
+
+  const chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['2018', 2,3,4,5,6]
+      ]
+    }
+  });
 }
