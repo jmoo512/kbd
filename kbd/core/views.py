a0c184c5a777c03745eaaf9d070b23d7406bcb3b
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-05-07 17:23:13 -0500

    index links and redirects

--- kbd/core/views.py
+++ kbd/core/views.py
@@ -42,15 +42,14 @@ def store_weekly():
 @core.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
-        return redirect(url_for('core.plotly'))
+        return redirect(url_for('core.index'))
     form = LoginForm()
     if form.validate_on_submit():
         user = Users.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
             return redirect(url_for('core.login'))
         login_user(user, remember=form.remember_me.data)
-        return redirect(url_for('core.plotly'))
+        return redirect(url_for('core.index'))
     return render_template('login.html', title='Sign In', form=form)
 
 @core.route('/logout')
