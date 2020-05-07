d65b1fe5e0cb4aa1efe42f15241cbf2c1d229ab7
knjmoo <57110372+knjmoo@users.noreply.github.com>
2020-05-07 17:34:05 -0500

    forms and style

--- kbd/core/views.py
+++ kbd/core/views.py
@@ -48,7 +48,7 @@ def login():
         user = Users.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             return redirect(url_for('core.login'))
-        login_user(user, remember=form.remember_me.data)
+        login_user(user)
         return redirect(url_for('core.index'))
     return render_template('login.html', title='Sign In', form=form)
 
