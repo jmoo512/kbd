e0c6fa844a5a687af211327a7cfca45cd9818ed2
jmoo <joshmoore@wesellmeat.com>
2020-06-18 01:37:57 -0500

    sales

--- kbd/static/sales.js
+++ kbd/static/sales.js
@@ -1,5 +1,3 @@
-import {colors, ranges, inspLayout, gfLayout, config, staticConfig} from './modules/chartConfig.js'
-
 //Add event listener to selector to call update functions
 
 document.getElementById("store-select").addEventListener("change",updateCharts);
@@ -148,6 +146,18 @@ async function getTotalCumul() {
 }
 
 
+//colors object
+let colors = {
+  "red":"#ac3e31",
+  "yellow":"#dbae58",
+  "blue":"#488a99",
+  "bgColor":"#dadada",
+  "text":"#20283e",
+  "grey":"#484848",
+  "2018":"#ac3e31",
+  "2019":"#dbae58",
+  "2020":"#47ca3e"
+  }
 
 //default layout for charts
 
@@ -155,13 +165,13 @@ let layout =  {
   autosize: true,
   paper_bgcolor: colors['grey'],
   plot_bgcolor: colors['grey'],
-  //width: 500,
+  width: 500,
   height: 350,
   margin: {
-    l: 30,
-    r: 10,
-    b: 10,
-    t: 10,
+    l: 50,
+    r: 50,
+    b: 50,
+    t: 50,
     pad: 5
   },
   xaxis: {
@@ -185,6 +195,10 @@ let layout =  {
       color: '#FFF'
     }}
 }
+
+let config = {responsive: true, displayModeBar: false}
+
+
 
 //update store level charts based on selected store
 async function updateCharts () {
@@ -384,7 +398,7 @@ async function populateBaseCharts() {
     text: pctSales
   };
 
-  let totalSales = [sales18, sales19, sales20]
+  totalSales = [sales18, sales19, sales20]
 
   let gc19 = {
     x: weeks,
@@ -409,7 +423,7 @@ async function populateBaseCharts() {
     text: pctGC
   };
 
-  let totalGC = [gc19, gc20]
+  totalGC = [gc19, gc20]
 
   let cumul18 = {
     x: weeks,
@@ -445,7 +459,7 @@ async function populateBaseCharts() {
     text: pctCumul
   };
 
-  let totalCumul = [cumul18, cumul19, cumul20]
+  totalCumul = [cumul18, cumul19, cumul20]
 
 
   Plotly.newPlot( 'total-sales-chart', totalSales, layout, config);
